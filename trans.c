e8f997c025053a6e0eac21b441ba4cdf84a17a38
Anu-cse <apanushree4@gmail.com>
2026-05-02 10:05:00 +0530

    account

--- trans.c
+++ trans.c
@@ -8,28 +8,52 @@ struct clientData {
     double balance;
 };
 
+void addAccount(FILE *fPtr);
+
 int main() {
 
     FILE *cfPtr;
 
     if ((cfPtr = fopen("credit.dat", "rb+")) == NULL) {
         cfPtr = fopen("credit.dat", "wb+");
-        if (cfPtr == NULL) {
-            printf("File could not be opened.\n");
-            return 1;
-        }
     }
 
     int choice;
 
     do {
         printf("\n--- BANK SYSTEM ---\n");
-        printf("1. Exit\n");
+        printf("1. Add Account\n");
+        printf("2. Exit\n");
         printf("Enter choice: ");
         scanf("%d", &choice);
 
-    } while (choice != 1);
+        if (choice == 1)
+            addAccount(cfPtr);
+
+    } while (choice != 2);
 
     fclose(cfPtr);
     return 0;
+}
+
+void addAccount(FILE *fPtr) {
+
+    struct clientData client;
+
+    printf("Enter Account Number: ");
+    scanf("%u", &client.acctNum);
+
+    printf("Enter Last Name: ");
+    scanf("%s", client.lastName);
+
+    printf("Enter First Name: ");
+    scanf("%s", client.firstName);
+
+    printf("Enter Balance: ");
+    scanf("%lf", &client.balance);
+
+    fseek(fPtr, (client.acctNum - 1) * sizeof(struct clientData), SEEK_SET);
+    fwrite(&client, sizeof(struct clientData), 1, fPtr);
+
+    printf("Account added.\n");
 }
