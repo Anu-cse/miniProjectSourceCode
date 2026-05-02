2b04765ea259d91997af67ba1acabeb6958de803
Anu-cse <apanushree4@gmail.com>
2026-05-02 10:11:36 +0530

    text file

--- trans.c
+++ trans.c
@@ -216,8 +216,7 @@ void newRecord(FILE *fPtr)
 
 unsigned int enterChoice(void)
 {
-    unsigned int menuChoice; // variable to store user's choice
-    // display available options
+    unsigned int menuChoice; 
     printf("\n------------------------------\n");
     printf("%s", "\nEnter your choice\n"
                  "1 - store a formatted text file of accounts called\n"
