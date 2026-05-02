c95db2a703a6e461dbf5ddebbe23e50fc5c3b673
Anu-cse <apanushree4@gmail.com>
2026-05-02 10:07:53 +0530

    search

--- trans.c
+++ trans.c
@@ -1,72 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
+// SAME HEADERS + STRUCT
 
-struct clientData {
-    unsigned int acctNum;
-    char lastName[15];
-    char firstName[10];
-    double balance;
-};
+void searchAccount(FILE *fPtr);
 
-void addAccount(FILE *fPtr);
-void displayAll(FILE *fPtr);
+// inside main add option 3
 
-int main() {
-
-    FILE *cfPtr;
-
-    if ((cfPtr = fopen("credit.dat", "rb+")) == NULL) {
-        cfPtr = fopen("credit.dat", "wb+");
-    }
-
-    int choice;
-
-    do {
-        printf("\n--- BANK SYSTEM ---\n");
-        printf("1. Add Account\n");
-        printf("2. Display All\n");
-        printf("3. Exit\n");
-        printf("Enter choice: ");
-        scanf("%d", &choice);
-
-        if (choice == 1) addAccount(cfPtr);
-        else if (choice == 2) displayAll(cfPtr);
-
-    } while (choice != 3);
-
-    fclose(cfPtr);
-    return 0;
-}
-
-void addAccount(FILE *fPtr) {
+void searchAccount(FILE *fPtr) {
     struct clientData client;
+    unsigned int acc;
+    int found = 0;
 
     printf("Enter Account Number: ");
-    scanf("%u", &client.acctNum);
-    printf("Enter Last Name: ");
-    scanf("%s", client.lastName);
-    printf("Enter First Name: ");
-    scanf("%s", client.firstName);
-    printf("Enter Balance: ");
-    scanf("%lf", &client.balance);
+    scanf("%u", &acc);
 
-    fseek(fPtr, (client.acctNum - 1) * sizeof(struct clientData), SEEK_SET);
-    fwrite(&client, sizeof(struct clientData), 1, fPtr);
-}
-
-void displayAll(FILE *fPtr) {
-    struct clientData client;
     rewind(fPtr);
 
-    printf("\nAcct  Last  First  Balance\n");
-
     while (fread(&client, sizeof(client), 1, fPtr)) {
-        if (client.acctNum != 0) {
+        if (client.acctNum == acc) {
             printf("%u %s %s %.2f\n",
                    client.acctNum,
                    client.lastName,
                    client.firstName,
                    client.balance);
+            found = 1;
+            break;
         }
     }
+
+    if (!found)
+        printf("Not found\n");
 }
